6004fc36ade3a617a69aef9dcf6aa7cc21b2c88b
tobias-fletcher <>
2021-05-01 22:38:29 -0500

    updated code

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
 export class MovieCard extends React.Component {
+
   render() {
     const { movie, onMovieClick } = this.props;
-
-    return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+    return <div onClick={() => { onMovieClick(movie) }} className="movie-card"> {movie.Title} </div>;
   }
-}
