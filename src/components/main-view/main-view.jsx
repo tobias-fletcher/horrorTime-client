2b37eff4332fb61c84c4a29a4c8ab3a6fe90c713
tobias-fletcher <75866568+tobias-fletcher@users.noreply.github.com>
2021-05-03 09:34:24 -0500

    updated directories

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { MovieCard } from '../movieCard/movieCard';
-import { MovieView } from '../movieView/movieView';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 
 export class MainView extends React.Component {
