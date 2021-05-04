e910d1c46797064fdb7a3fd09b7d616e65daadb4
tobias-fletcher <>
2021-05-04 18:49:07 -0500

    updated index.jsx
    - added index.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -15,7 +15,7 @@ export class MainView extends React.Component {
   }
 
   componentDidMount() {
-    axios.get('https://[APP-NAME].herokuapp.com/movies')
+    axios.get('https://horror-time.herokuapp.com/movies')
       .then(response => {
         this.setState({
           movies: response.data
