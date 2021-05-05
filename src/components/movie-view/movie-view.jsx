0946abf98cb6765db9d5f2004c5f9f3cf8539355
tobias-fletcher <>
2021-05-04 19:27:46 -0500

    added key event listened movie-view
    added key event listener to movie-view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 export class MovieView extends React.Component {
 
+  keypressCallback(event) {
+    console.log(event.key);
+  }
   componentDidMount() {
     document.addEventListener('keypress', event => {
       console.log(event.key);
