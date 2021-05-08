06e48f4f743d87a0828c5fb34f99122546fe760a
tobias-fletcher <>
2021-05-07 19:39:05 -0500

    updated login endpoint
    added login endpoint

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -8,7 +8,7 @@ export function LoginView(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(username, password);
-    axios.post('/login', {
+    axios.post('localhost:5000/login', {
       Uername: username,
       Password: password
     })
