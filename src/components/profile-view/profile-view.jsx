8b78fe7799b749f41d30a1344476ee838a1a440b
tobias-fletcher <75866568+tobias-fletcher@users.noreply.github.com>
2021-05-23 18:02:43 -0500

    made one change to proile-view
    requesting assistance with put method

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -14,8 +14,13 @@ import Link, { useParams } from 'react-router-dom';
 
 export class ProfileView extends React.Component {
 
-
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      newUsername: '',
+      setNewUsername: ''
+    }
+  }
   componentDidUpdate(prevProps) {
     if (this.props.Username !== prevProps.Username) {
       this.fetchData(this.props.Username);
@@ -29,74 +34,74 @@ export class ProfileView extends React.Component {
       Username: this.state.username
     }
   }
-}
-
-handleUpdate() {
-  const user = user;
-  axios.put(`https://itshorrortime.herokuapp.com/users/${user}`, {
-    Username: newUsername,
-  }).then((response) => {
-    const data = response.data;
-    this.onUpdate(data);
-    console.log(response);
-  });
-
-}
 
 
-render() {
+  handleUpdate() {
+    const user = user;
+    axios.put(`https://itshorrortime.herokuapp.com/users/${user}`, {
+      Username: newUsername,
+    }).then((response) => {
+      const data = response.data;
+      this.onUpdate(data);
+      console.log(response);
+    });
 
-  const { user, token, userInfo } = this.props;
-
-  console.log(this.props);
-
-
-  return (
-    <>
-      <Helmet bodyAttributes={{ style: 'background-color : black, text-decoration-color : white' }} />
-      <Card>
-        <Card.Title> Account Information</Card.Title>
-        <Card.Body>
-          <Card.Text>Username: {user}</Card.Text>
-          <Card.Text>Birthdate: {userInfo.Birthday}</Card.Text>
-          <Card.Text>Email: {userInfo.Email}</Card.Text>
-        </Card.Body>
-
-        <Card.Body>
-          {/*make drop down when clicking update information to pull up update form*/}
-
-          <Button>Update Information</Button>
-
-        </Card.Body>
-
-      </Card>
-      <Form.Row className="justify-content-center mt-5">
-
-        <Form>
-          <Form.Row className="justify-content-center">
-            <Form.Group as={Row} controlId='validationCustomUsername'>
-              <Form.Label>Username:</Form.Label>
-              <Form.Control
-                type='text'
-                required
-                minLength="6"
-                maxLength="12"
-                placeholder='Enter Username'
-
-              />
-
-            </Form.Group>
-
-          </Form.Row>
-          <Button onClick={handleUpdate} variant="dark" block >Submit</Button>
-        </Form>
-      </Form.Row>
-
-    </>
-  )
+  }
 
 
-}
+  render() {
+
+    const { user, token, userInfo } = this.props;
+
+    console.log(this.props);
+
+
+    return (
+      <>
+        <Helmet bodyAttributes={{ style: 'background-color : black, text-decoration-color : white' }} />
+        <Card>
+          <Card.Title> Account Information</Card.Title>
+          <Card.Body>
+            <Card.Text>Username: {user}</Card.Text>
+            <Card.Text>Birthdate: {userInfo.Birthday}</Card.Text>
+            <Card.Text>Email: {userInfo.Email}</Card.Text>
+          </Card.Body>
+
+          <Card.Body>
+            {/*make drop down when clicking update information to pull up update form*/}
+
+            <Button>Update Information</Button>
+
+          </Card.Body>
+
+        </Card>
+        <Form.Row className="justify-content-center mt-5">
+
+          <Form>
+            <Form.Row className="justify-content-center">
+              <Form.Group as={Row} controlId='validationCustomUsername'>
+                <Form.Label>Username:</Form.Label>
+                <Form.Control
+                  type='text'
+                  required
+                  minLength="6"
+                  maxLength="12"
+                  placeholder='Enter Username'
+                  onChange={handleUpdate}
+                />
+
+              </Form.Group>
+
+            </Form.Row>
+            <Button onClick={handleUpdate} variant="dark" block >Submit</Button>
+          </Form>
+        </Form.Row>
+
+      </>
+    )
+
+
+  }
 }
 
 
